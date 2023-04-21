6ed800ce55b79f77a69255cc350fab67dbc34532
cccaaannn <can.kurt.aa@gmail.com>
2023-04-22 00:23:53 +0300

    help and about commands added

--- telegram_chatgpt/application/telegram_application.py
+++ telegram_chatgpt/application/telegram_application.py
@@ -1,3 +1,4 @@
+import textwrap
 import logging
 import sys
 import os
@@ -41,6 +42,34 @@ class TelegramApplication():
             except:
                 self.__logger.error("Could not inform user for exception", exc_info=context.error)
 
+        async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+            self.__logger.info(f"User: {update.message.from_user}")
+
+            help_text = textwrap.dedent(f"""\
+                [Commands]
+
+                [Chat]
+                /reset (Resets conversation history)
+                /regen or /regenerate (Regenerates last response)
+
+                [Utilities]
+                /about
+                /help
+                """)
+            await update.message.reply_text(help_text)
+
+        async def about_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+            self.__logger.info(f"User: {update.message.from_user}")
+
+            help_text = textwrap.dedent("""\
+                [About]
+
+                Telegram ChatGPT is an open source project visit projects GitHub page
+                https://github.com/cccaaannn/telegram_chatgpt
+
+                Author Can Kurt""")
+            await update.message.reply_text(help_text)
+
         async def reset_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             self.__logger.info(f"User: {update.message.from_user}")
 
@@ -70,6 +99,9 @@ class TelegramApplication():
         application.add_handler(CommandHandler("reset", reset_command))
         application.add_handler(CommandHandler("regen", regenerate_answer_command))
         application.add_handler(CommandHandler("regenerate", regenerate_answer_command))
+
+        application.add_handler(CommandHandler("help", help_command))
+        application.add_handler(CommandHandler("about", about_command))
 
         application.add_handler(MessageHandler(
             filters.TEXT & ~filters.COMMAND, complete_chat))
